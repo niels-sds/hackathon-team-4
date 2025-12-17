a6ce22f0b3ef0f009d5ed5e59c534047871d451d
Anton Ishchyk <Anton.ishchyk@scordigitalsolutions.com>
2025-12-17 11:22:03 +0100

    fix: revert chat model name to chatmodel in foundry_models.tf

--- infra/foundry_models.tf
+++ infra/foundry_models.tf
@@ -1,6 +1,6 @@
 resource "azapi_resource" "chat_model_deployment" {
   type      = "Microsoft.CognitiveServices/accounts/deployments@2025-04-01-preview"
-  name      = "gpt-5-chat"
+  name      = "chatmodel"
   parent_id = azapi_resource.foundry.id
   tags      = local.tags_azapi
   body = {
